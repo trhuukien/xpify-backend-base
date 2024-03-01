bac53ecc6651a6d9a47f4b625859e8afbbb8e9d9
lucthanca <lucthanca@gmail.com>
2024-03-01 14:48:27 +0700

    docs

--- app/code/Xpify/Core/GraphQl/Query/ErrorHandler.php
+++ app/code/Xpify/Core/GraphQl/Query/ErrorHandler.php
@@ -3,6 +3,13 @@ declare(strict_types=1);
 
 namespace Xpify\Core\GraphQl\Query;
 
+/**
+ * This class was born to handle the error when the access token is expired.
+ * Default magento core graphql just gap the error -> push it to the error array in the response.
+ * But we need to throw the exception when the access token is expired. to handle redirection in frontend.
+ * This will present in the rewritten GraphQl controller.
+ * @see \Xpify\Core\GraphQl\Controller\GraphQl
+ */
 class ErrorHandler extends \Magento\Framework\GraphQl\Query\ErrorHandler
 {
     /**
