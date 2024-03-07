c05e7fd27836b742ab3b60012c812d03b5362a2e
Kiz <kienth@bsscommerce.com>
2024-03-07 15:18:35 +0700

    Fix conflict

--- app/code/SectionBuilder/Faq/registration.php
+++ app/code/SectionBuilder/Faq/registration.php
@@ -3,10 +3,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-<<<<<<< HEAD:app/code/SectionBuilder/Faq/registration.php
     'SectionBuilder_Faq',
-=======
-    'Xpify_Asset',
->>>>>>> master:app/code/Xpify/Asset/registration.php
     __DIR__
 );
--- app/code/SectionBuilder/Tag/registration.php
+++ app/code/SectionBuilder/Tag/registration.php
@@ -3,10 +3,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-<<<<<<< HEAD:app/code/SectionBuilder/Tag/registration.php
     'SectionBuilder_Tag',
-=======
-    'Xpify_Theme',
->>>>>>> master:app/code/Xpify/Theme/registration.php
     __DIR__
 );
