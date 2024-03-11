1fc4df42f29c5897f8c0ba3abfc9a2309c441f50
lucthanca <lucthanca@gmail.com>
2024-03-11 15:49:14 +0700

    handle 404 section, bundle

--- app/code/SectionBuilder/ProductGraphQl/Model/Resolver/SectionTypeResolver.php
+++ app/code/SectionBuilder/ProductGraphQl/Model/Resolver/SectionTypeResolver.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace SectionBuilder\ProductGraphQl\Model\Resolver;
 
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\GraphQl\Query\Resolver\TypeResolverInterface;
 
 class SectionTypeResolver implements TypeResolverInterface
@@ -12,8 +13,15 @@ class SectionTypeResolver implements TypeResolverInterface
         "2" => "GroupSection",
     ];
 
+    /**
+     * @inheritDoc
+     * @throws GraphQlNoSuchEntityException
+     */
     public function resolveType(array $data): string
     {
-        return self::TYPE_MAPPING[$data['type_id']] ?? 'SectionInterface';
+        if (empty($data['type_id']) || empty(self::TYPE_MAPPING[$data['type_id']])) {
+            throw new GraphQlNoSuchEntityException(__('Section not found'));
+        }
+        return self::TYPE_MAPPING[$data['type_id']];
     }
 }
